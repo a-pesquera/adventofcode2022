23dc91530424dcce082908b261bf0e4a8d56ccdc
Alberto Pesquera <albertopesquera1992@gmail.com>
2022-12-10 14:21:09 +0100

    Day 10 Part 2

--- days/day10.py
+++ days/day10.py
@@ -4,11 +4,12 @@ DATA_FILE = 'day10.txt'
 EXAMPLE_FILE = 'day10-example.txt'
 
 
-def sum_of_signals(data, cycle_offset=20, cycle_freq=40):
+def do_cpu(data, cycle_offset=20, cycle_freq=40, crt_row=40):
     cycle = 0
     registry_x = 1
 
     signals = []
+    crt = ''
 
     for line in data:
         operations = []
@@ -28,11 +29,30 @@ def sum_of_signals(data, cycle_offset=20, cycle_freq=40):
             if (cycle - cycle_offset) % cycle_freq == 0:
                 signals.append(cycle * registry_x)
 
+            # Render CRT
+            is_pixel_light = registry_x - 1 <= (cycle - 1) % crt_row <= registry_x + 1
+            crt += '#' if is_pixel_light else '.'
+
             # End of cycle
             if op[0] == 'addx':
                 registry_x += op[1]
 
-    return sum(signals)
+    chunks = []
+    while crt:
+        chunk = crt[:crt_row]
+        crt = crt[crt_row:]
+        chunks.append(chunk)
+    return sum(signals), chunks
+
+
+def sum_of_signals(data, cycle_offset=20, cycle_freq=40):
+    sum_signals, _ = do_cpu(data, cycle_offset=cycle_offset, cycle_freq=cycle_freq)
+    return sum_signals
+
+
+def crt_output(data, crt_row=40):
+    _, crt_output = do_cpu(data, crt_row=crt_row)
+    return crt_output
 
 
 def part_1(input_file):
@@ -41,4 +61,5 @@ def part_1(input_file):
 
 
 def part_2(input_file):
-    raise NotImplementedError
+    data = common.read_data_file_generator(input_file)
+    return crt_output(data)
--- tests/test_day10.py
+++ tests/test_day10.py
@@ -38,10 +38,95 @@ class TestDay10:
         result = day.part_1(day.DATA_FILE)
         assert result == 14420
 
-    def _test_part_2_example(self):
-        result = day.part_2(day.EXAMPLE_FILE)
-        assert result == 0
+    def test_part_2_example_noop(self):
+        data = (x for x in [
+            'noop',
+            'noop',
+            'noop',
+            'noop',
+            'noop',
+            'noop',
+            'noop',
+            'noop',
+        ])
+        result = day.crt_output(data)
+        assert result == ['###.....']
 
-    def _test_part_2(self):
+    def test_part_2_example_small(self):
+        data = (x for x in [
+            'noop',
+            'addx 3',
+            'addx -5',
+        ])
+        result = day.crt_output(data)
+        assert result == ['#####']
+
+    def test_part_2_example_part(self):
+        data = (x for x in [
+            'addx 15',
+            'addx -11',
+            'addx 6',
+            'addx -3',
+            'addx 5',
+            'addx -1',
+            'addx -8',
+            'addx 13',
+            'addx 4',
+            'noop',
+            'addx -1',
+        ])
+        result = day.crt_output(data)
+        assert result == ['##..##..##..##..##..#']
+
+    def test_part_2_example_until_row_2(self):
+        data = (x for x in [
+            'addx 15',
+            'addx -11',
+            'addx 6',
+            'addx -3',
+            'addx 5',
+            'addx -1',
+            'addx -8',
+            'addx 13',
+            'addx 4',
+            'noop',
+            'addx -1',
+            'addx 5',
+            'addx -1',
+            'addx 5',
+            'addx -1',
+            'addx 5',
+            'addx -1',
+            'addx 5',
+            'addx -1',
+            'addx -35',
+            'addx 1',
+        ])
+        result = day.crt_output(data)
+        assert result == [
+            '##..##..##..##..##..##..##..##..##..##..',
+            '#',
+        ]
+
+
+    def test_part_2_example(self):
+        result = day.part_2(day.EXAMPLE_FILE)
+        assert result == [
+            '##..##..##..##..##..##..##..##..##..##..',
+            '###...###...###...###...###...###...###.',
+            '####....####....####....####....####....',
+            '#####.....#####.....#####.....#####.....',
+            '######......######......######......####',
+            '#######.......#######.......#######.....',
+        ]
+
+    def test_part_2(self):
         result = day.part_2(day.DATA_FILE)
-        assert result == 0
+        assert result == [
+            '###...##..#....###..###..####..##..#..#.',
+            '#..#.#..#.#....#..#.#..#....#.#..#.#..#.',
+            '#..#.#....#....#..#.###....#..#..#.#..#.',
+            '###..#.##.#....###..#..#..#...####.#..#.',
+            '#.#..#..#.#....#.#..#..#.#....#..#.#..#.',
+            '#..#..###.####.#..#.###..####.#..#..##..',
+        ]
