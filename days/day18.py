317911ec54968c83d460c892c2c2b08ebcc74eb9
Alberto Pesquera <albertopesquera1992@gmail.com>
2022-12-18 13:18:29 +0100

    Day 18 Part 2

--- days/day18.py
+++ days/day18.py
@@ -22,29 +22,172 @@ def are_adjacent(cube1, cube2):
     return abs(cube1[0] - cube2[0]) + abs(cube1[1] - cube2[1]) + abs(cube1[2] - cube2[2]) == 1
 
 
-def foo(data):
+def count_surfaced_area(data):
     cubes = []
     for line in data:
         actual_cube = parse_line(line)
-        print('actual_cube', actual_cube)
+        # print('actual_cube', actual_cube)
         exposed_sides = 6
         for cube_row in cubes:
             cube = cube_row[0]
-            print('cube', cube)
+            # print('cube', cube)
             if are_adjacent(actual_cube, cube):
                 exposed_sides -= 1
                 cube_row[1] -= 1
         cubes.append([actual_cube, exposed_sides])
 
-    print(cubes)
+    # print(cubes)
 
     return sum(x[1] for x in cubes)
 
 
+def find_mins_and_maxes(droplet):
+    initial_cube = next(iter(droplet))
+    min_x = max_x = initial_cube[0]
+    min_y = max_y = initial_cube[1]
+    min_z = max_z = initial_cube[2]
+
+    for cube in droplet:
+        min_x = min(min_x, cube[0])
+        max_x = max(max_x, cube[0])
+
+        min_y = min(min_y, cube[1])
+        max_y = max(max_y, cube[1])
+
+        min_z = min(min_z, cube[2])
+        max_z = max(max_z, cube[2])
+
+    return {
+        'min_x': min_x,
+        'max_x': max_x,
+        'min_y': min_y,
+        'max_y': max_y,
+        'min_z': min_z,
+        'max_z': max_z,
+    }
+
+
+def point_adjacent_to_droplet(cube, droplet):
+    candidates = [
+        (cube[0], cube[1], cube[2] + 1),
+        (cube[0], cube[1], cube[2] - 1),
+        (cube[0], cube[1] + 1, cube[2]),
+        (cube[0], cube[1] - 1, cube[2]),
+        (cube[0] + 1, cube[1], cube[2]),
+        (cube[0] - 1, cube[1], cube[2]),
+    ]
+
+    return [p for p in candidates if p in droplet]
+
+
+def point_is_exterior(cube, droplet, mins_and_maxes):
+    # First, x from left to right
+    for x in range(mins_and_maxes['min_x'], cube[0]):
+        test_point = (x, cube[1], cube[2])
+        if test_point in droplet:
+            break
+    else:
+        return True
+
+    # Second, x from right to left
+    for x in range(cube[0] + 1, mins_and_maxes['max_x'] + 1):
+        test_point = (x, cube[1], cube[2])
+        if test_point in droplet:
+            break
+    else:
+        return True
+
+    # Then the same for y
+    for y in range(mins_and_maxes['min_y'], cube[1]):
+        test_point = (cube[0], y, cube[2])
+        if test_point in droplet:
+            break
+    else:
+        return True
+
+    for y in range(cube[1] + 1, mins_and_maxes['max_y'] + 1):
+        test_point = (cube[0], y, cube[2])
+        if test_point in droplet:
+            break
+    else:
+        return True
+
+    # And finally for z
+    for z in range(mins_and_maxes['min_z'], cube[2]):
+        test_point = (cube[0], cube[1], z)
+        if test_point in droplet:
+            break
+    else:
+        return True
+
+    for z in range(cube[2] + 1, mins_and_maxes['max_z'] + 1):
+        test_point = (cube[0], cube[1], z)
+        if test_point in droplet:
+            break
+    else:
+        return True
+
+    return False
+
+
+def count_exterior_surfaced_area(data):
+    cubes = []
+    droplet = set()
+    for line in data:
+        actual_cube = parse_line(line)
+        droplet.add(actual_cube)
+
+    print('droplet', droplet)
+    mins_and_maxes = find_mins_and_maxes(droplet)
+    print('mins_and_maxes', mins_and_maxes)
+
+    wrapper_cube = []  # [x][y][z]
+    wrapper_x_from = mins_and_maxes['min_x'] - 1
+    wrapper_x_to = mins_and_maxes['max_x'] + 1
+    wrapper_y_from = mins_and_maxes['min_y'] - 1
+    wrapper_y_to = mins_and_maxes['max_y'] + 1
+    wrapper_z_from = mins_and_maxes['min_z'] - 1
+    wrapper_z_to = mins_and_maxes['max_z'] + 1
+
+    result = 0
+    for x in range(wrapper_x_from, wrapper_x_to + 1):
+        # print('x', x)
+        for y in range(wrapper_y_from, wrapper_y_to + 1):
+            # print('y', y)
+            for z in range(wrapper_z_from, wrapper_z_to + 1):
+                # print('z', z)
+                point = (x, y, z)
+                # print('point', point)
+
+                if point in droplet:
+                    continue
+
+                cubes_adjacents = point_adjacent_to_droplet(point, droplet)
+                if not cubes_adjacents:
+                    continue
+
+                print('point', point)
+
+                is_exterior = point_is_exterior(point, droplet, mins_and_maxes)
+                if not is_exterior:
+                    print('Not exterior point')
+                    continue
+
+                print('Exterior point!!')
+                print('Adjacent to:', cubes_adjacents)
+                result += len(cubes_adjacents)
+
+    # print('droplet', droplet)
+    # print('mins_and_maxes', mins_and_maxes)
+
+    return result
+
+
 def part_1(input_file):
     data = common.read_data_file_generator(input_file)
-    return foo(data)
+    return count_surfaced_area(data)
 
 
 def part_2(input_file):
-    raise NotImplementedError
+    data = common.read_data_file_generator(input_file)
+    return count_exterior_surfaced_area(data)
--- tests/test_day18.py
+++ tests/test_day18.py
@@ -2,6 +2,152 @@ import days.day18 as day
 
 
 class TestDay18:
+    SMALL_EXAMPLE = ['1,1,1', '2,1,1']
+    SMALL_EXAMPLE_EXTENDED = ['1,1,1', '2,1,1', '3,1,1']
+    SMALL_SHAPE_V = ['1,1,1', '2,1,1', '1,2,1']
+    ONE_HOLLOW = ['0,0,1', '0,0,-1', '0,1,0', '0,-1,0', '1,0,0', '-1,0,0']
+    NO_HOLLOW = ['0,0,0', '0,0,1', '0,0,-1', '0,1,0', '0,-1,0', '1,0,0', '-1,0,0']
+    CUBE_SIZE_2 = [
+        '0,0,0',
+        '0,0,1',
+        '0,1,0',
+        '0,1,1',
+
+        '1,0,0',
+        '1,0,1',
+        '1,1,0',
+        '1,1,1',
+    ]
+    CUBE_SIZE_3 = [
+        '0,0,0',
+        '0,0,1',
+        '0,0,2',
+        '0,1,0',
+        '0,1,1',
+        '0,1,2',
+        '0,2,0',
+        '0,2,1',
+        '0,2,2',
+
+        '1,0,0',
+        '1,0,1',
+        '1,0,2',
+        '1,1,0',
+        '1,1,1',
+        '1,1,2',
+        '1,2,0',
+        '1,2,1',
+        '1,2,2',
+
+        '2,0,0',
+        '2,0,1',
+        '2,0,2',
+        '2,1,0',
+        '2,1,1',
+        '2,1,2',
+        '2,2,0',
+        '2,2,1',
+        '2,2,2',
+    ]
+    CUBE_SIZE_3_HOLLOW = [
+        '0,0,0',
+        '0,0,1',
+        '0,0,2',
+        '0,1,0',
+        '0,1,1',
+        '0,1,2',
+        '0,2,0',
+        '0,2,1',
+        '0,2,2',
+
+        '1,0,0',
+        '1,0,1',
+        '1,0,2',
+        '1,1,0',
+        # '1,1,1',
+        '1,1,2',
+        '1,2,0',
+        '1,2,1',
+        '1,2,2',
+
+        '2,0,0',
+        '2,0,1',
+        '2,0,2',
+        '2,1,0',
+        '2,1,1',
+        '2,1,2',
+        '2,2,0',
+        '2,2,1',
+        '2,2,2',
+    ]
+    SHAPE_U = [
+        '0,0,0',
+        '0,0,1',
+        '0,0,2',
+        '0,1,0',
+        # '0,1,1',
+        # '0,1,2',
+        '0,2,0',
+        '0,2,1',
+        '0,2,2',
+
+        '1,0,0',
+        '1,0,1',
+        '1,0,2',
+        '1,1,0',
+        # '1,1,1',
+        # '1,1,2',
+        '1,2,0',
+        '1,2,1',
+        '1,2,2',
+
+        '2,0,0',
+        '2,0,1',
+        '2,0,2',
+        '2,1,0',
+        # '2,1,1',
+        # '2,1,2',
+        '2,2,0',
+        '2,2,1',
+        '2,2,2',
+    ]
+
+    def test_count_surfaced_area_small_example(self):
+        result = day.count_surfaced_area((x for x in self.SMALL_EXAMPLE))
+        assert result == 10
+
+    def test_count_surfaced_area_small_example_extended(self):
+        result = day.count_surfaced_area((x for x in self.SMALL_EXAMPLE_EXTENDED))
+        assert result == 14
+
+    def test_count_surfaced_area_small_shape_v(self):
+        result = day.count_surfaced_area((x for x in self.SMALL_SHAPE_V))
+        assert result == 14
+
+    def test_count_surfaced_area_one_hollow(self):
+        result = day.count_surfaced_area((x for x in self.ONE_HOLLOW))
+        assert result == 36
+
+    def test_count_surfaced_area_no_hollow(self):
+        result = day.count_surfaced_area((x for x in self.NO_HOLLOW))
+        assert result == 30
+
+    def test_count_surfaced_area_cube_size_2(self):
+        result = day.count_surfaced_area((x for x in self.CUBE_SIZE_2))
+        assert result == 24
+
+    def test_count_surfaced_area_cube_size_3(self):
+        result = day.count_surfaced_area((x for x in self.CUBE_SIZE_3))
+        assert result == 54
+
+    def test_count_surfaced_area_cube_size_3_hollow(self):
+        result = day.count_surfaced_area((x for x in self.CUBE_SIZE_3_HOLLOW))
+        assert result == 60
+
+    def test_count_surfaced_area_shape_u(self):
+        result = day.count_surfaced_area((x for x in self.SHAPE_U))
+        assert result == 62
+
     def test_part_1_example(self):
         result = day.part_1(day.EXAMPLE_FILE)
         assert result == 64
@@ -10,10 +156,46 @@ class TestDay18:
         result = day.part_1(day.DATA_FILE)
         assert result == 3326
 
-    def _test_part_2_example(self):
-        result = day.part_2(day.EXAMPLE_FILE)
-        assert result == 0
+    def test_count_exterior_surfaced_area_small_example(self):
+        result = day.count_exterior_surfaced_area((x for x in self.SMALL_EXAMPLE))
+        assert result == 10
 
-    def _test_part_2(self):
+    def test_count_exterior_surfaced_area_small_example_extended(self):
+        result = day.count_exterior_surfaced_area((x for x in self.SMALL_EXAMPLE_EXTENDED))
+        assert result == 14
+
+    def test_count_exterior_surfaced_area_small_shape_v(self):
+        result = day.count_exterior_surfaced_area((x for x in self.SMALL_SHAPE_V))
+        assert result == 14
+
+    def test_count_exterior_surfaced_area_one_hollow(self):
+        result = day.count_exterior_surfaced_area((x for x in self.ONE_HOLLOW))
+        assert result == 30
+
+    def test_count_exterior_surfaced_area_no_hollow(self):
+        result = day.count_exterior_surfaced_area((x for x in self.NO_HOLLOW))
+        assert result == 30
+
+    def test_count_exterior_surfaced_area_cube_size_2(self):
+        result = day.count_exterior_surfaced_area((x for x in self.CUBE_SIZE_2))
+        assert result == 24
+
+    def test_count_exterior_surfaced_area_cube_size_3(self):
+        result = day.count_exterior_surfaced_area((x for x in self.CUBE_SIZE_3))
+        assert result == 54
+
+    def test_count_exterior_surfaced_area_cube_size_3_hollow(self):
+        result = day.count_exterior_surfaced_area((x for x in self.CUBE_SIZE_3_HOLLOW))
+        assert result == 54
+
+    def test_count_exterior_surfaced_area_shape_u(self):
+        result = day.count_exterior_surfaced_area((x for x in self.SHAPE_U))
+        assert result == 62
+
+    def test_part_2_example(self):
+        result = day.part_2(day.EXAMPLE_FILE)
+        assert result == 58
+
+    def test_part_2(self):
         result = day.part_2(day.DATA_FILE)
-        assert result == 0
+        assert result == 1996
