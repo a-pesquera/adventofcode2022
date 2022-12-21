0a2db53a9a6d8931b7e1f8acf13564d4141bd856
Alberto Pesquera <albertopesquera1992@gmail.com>
2022-12-21 20:49:14 +0100

    Day 21 Part 2

--- days/day21.py
+++ days/day21.py
@@ -4,34 +4,131 @@ DATA_FILE = 'day21.txt'
 EXAMPLE_FILE = 'day21-example.txt'
 
 
-def foo(data):
+def run_calculations(lst, variables):
+    has_new_variable = True
+    while has_new_variable:
+        has_new_variable = False
+
+        delete_from_lst_indexes = []
+        for i, (name, expression) in enumerate(lst):
+            if name in variables:
+                continue
+            try:
+                variables[name] = eval(expression, None, variables)
+                has_new_variable = True
+                delete_from_lst_indexes.append(i)
+            except NameError as e:
+                if e.args[0].startswith('name') and e.args[0].endswith('is not defined'):
+                    continue
+                raise e
+
+        # Delete already done expressions
+        for i in reversed(delete_from_lst_indexes):
+            lst.pop(i)
+
+
+def run_calculations_reversing_expressions(lst, variables):
+    # Invert explanation (second in tuple):
+    # "foo = bar * baz" transforms to "baz = foo / bar". Most of the cases new left is old equal.
+    # But "foo = bar / baz" transforms to "baz = bar / foo", so new *right* is equal, that's why
+    # the is_inverted is true only for "-" and "/".
+    operations_transform_left = {
+        '+': ('-', False),
+        '-': ('-', True),
+        '*': ('/', False),
+        '/': ('/', True),
+    }
+    operations_transform_right = {
+        '+': ('-', False),
+        '-': ('+', False),
+        '*': ('/', False),
+        '/': ('*', False),
+    }
+
+    while lst:
+        # Create new expressions
+        delete_from_lst_indexes = []
+        append_to_lst = []
+        for i, (name, expression) in enumerate(lst):
+            if name not in variables:
+                continue
+
+            left, operation, right = expression.split(' ')
+            if left in variables:
+                new_operation, is_inverted = operations_transform_left[operation]
+                new_name = right
+                new_left = name
+                new_right = left
+            else:
+                new_operation, is_inverted = operations_transform_right[operation]
+                new_name = left
+                new_left = name
+                new_right = right
+
+            new_expression = f'{new_left} {new_operation} {new_right}'
+            if is_inverted:
+                new_expression = f'{new_right} {new_operation} {new_left}'
+
+            delete_from_lst_indexes.append(i)
+            append_to_lst.append((new_name, new_expression))
+
+        # Delete transformed expressions
+        for i in reversed(delete_from_lst_indexes):
+            lst.pop(i)
+
+        # Generate new variables using new reversed expressions
+        lst.extend(append_to_lst)
+        run_calculations(lst, variables)
+
+
+def calculate_root_value(data):
     lst = []
     for line in data:
         name, expression = line.split(': ')
         lst.append((name, expression))
 
     variables = {}
-    pending = True
-    while pending:
-        pending = False
-        for name, expression in lst:
-            try:
-                variables[name] = eval(expression, None, variables)
-            except NameError as e:
-                if e.args[0].startswith('name') and e.args[0].endswith('is not defined'):
-                    pending = True
-                    continue
-                else:
-                    raise e
+    run_calculations(lst, variables)
 
-    print(variables)
     return variables['root']
+
+
+def calculate_humn_value(data):
+    lst = []
+
+    # Obtain root equal variables and ignore humn
+    root_parts = None
+    for line in data:
+        name, expression = line.split(': ')
+        if name == 'root':
+            left, *_, right = expression.split(' ')
+            root_parts = (left, right)
+            continue
+        elif name == 'humn':
+            continue
+        lst.append((name, expression))
+
+    # Like part 1 creating variables but not all will be set
+    variables = {}
+    run_calculations(lst, variables)
+
+    # Set root left-right value in variables
+    if root_parts[0] in variables:
+        variables[root_parts[1]] = variables[root_parts[0]]
+    else:
+        variables[root_parts[0]] = variables[root_parts[1]]
+
+    # Now go for the final result transforming the expressions
+    run_calculations_reversing_expressions(lst, variables)
+
+    return variables['humn']
 
 
 def part_1(input_file):
     data = common.read_data_file_generator(input_file)
-    return foo(data)
+    return calculate_root_value(data)
 
 
 def part_2(input_file):
-    raise NotImplementedError
+    data = common.read_data_file_generator(input_file)
+    return calculate_humn_value(data)
--- tests/test_day21.py
+++ tests/test_day21.py
@@ -10,10 +10,10 @@ class TestDay21:
         result = day.part_1(day.DATA_FILE)
         assert result == 66174565793494
 
-    def _test_part_2_example(self):
+    def test_part_2_example(self):
         result = day.part_2(day.EXAMPLE_FILE)
-        assert result == 0
+        assert result == 301
 
-    def _test_part_2(self):
+    def test_part_2(self):
         result = day.part_2(day.DATA_FILE)
-        assert result == 0
+        assert result == 3327575724809
